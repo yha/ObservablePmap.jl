1ebc23bc4a3982aac82ee5d4eef9d219b77ced31
yha <yharel@campus.technion.ac.il>
2020-12-14 18:48:33 +0200

    `Logging` integration

---
+++ src/ologpmap.jl
@@ -0,0 +1,67 @@
+using Logging
+
+struct ForwardLogger{L,IO,F} <: AbstractLogger
+    logger::L
+    io::IO
+    forwarder::F
+    function ForwardLogger(forwarder::F, logger_f, io::IO=IOBuffer()) where {F,IO}
+        logger = logger_f(io)
+        new{typeof(logger),IO,F}(logger, io, forwarder)
+    end
+end
+
+function Logging.handle_message(fwdl::ForwardLogger, args...; kwargs...)
+    Logging.handle_message(fwdl.logger, args...; kwargs...)
+    fwdl.forwarder(take!(fwdl.io))
+end
+Logging.min_enabled_level(fwdl::ForwardLogger) = Logging.min_enabled_level(fwdl.logger)
+Logging.shouldlog(fwdl::ForwardLogger, args...) = Logging.shouldlog(fwdl.logger, args...)
+
+"""
+    summary, task = ologpmap(f, [::AbstractWorkerPool], c...;
+                          logger_f=SimpleLogger, schedule_now=true,
+                          summarize=default_summarize,
+                          kwargs... )
+
+Transform collection(s) `c` by applying `f` to each element using available
+workers and tasks, by calling `Distributed.pmap`.
+The returned `summary` is an `Observable` containing a summary string of each
+worker's last log message (emitted by each worker using the `Logging` interface).
+The keyword argument `logger_f` is invoked with an `IOBuffer` internal to each
+worker to create a logger writing messages to this buffer. Log message read from
+this buffer are used as the worker's current status message included in the
+observable `summary`.
+
+The summary may be customized by setting the `summarize` keyword argument.
+
+The `ObservablePmap` module must be loaded on all workers before calling
+`ologpmap`, e.g. by `@everywhere using ObservablePmap`.
+
+Other positional and keyword arguments to `ologpmap` are the same as for `opmap`.
+
+Example:
+```jldoctest
+@everywhere using ObservablePmap
+@everywhere using ProgressLogging
+@everywhere using TerminalLoggers
+summ, task = ologpmap( 'a':'e', 2:2:10;
+                       on_error=identity, logger_f=TerminalLogger ) do c, x
+    @withprogress name="processing '$c'" for i=1:x
+        sleep(rand())
+        @logprogress i/x
+    end
+    @info "finished '$c'"
+    x
+end
+html = map(x -> HTML("<pre>$x</pre>"), summ)
+```
+"""
+ologpmap( f, c...; kwargs... ) = ologpmap( f, default_worker_pool(), c...; kwargs... )
+function ologpmap( f, p::AbstractWorkerPool, c...; logger_f=SimpleLogger, kwargs... )
+    opmap( p, c...; kwargs... ) do setmessage, x...
+        fwdlogger = ForwardLogger( setmessage∘String, logger_f )
+        with_logger(fwdlogger) do
+            f(x...)
+        end
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using ObservablePmap
 using Distributed
 using Observables
+using Logging
 
 addprocs(2)
 #addprocs(2; exeflags="--project")
@@ -11,43 +12,77 @@ addprocs(2)
 _summ( els, statuses, messages, ids ) = copy(messages)
 @testset "schedule_now=$schedule_now, worker_arg=$worker_arg" for
                     schedule_now in (false,true), worker_arg in (false,true)
+    function test_pmap_output(summ, task, v, r)
+        all_summaries = []
+        on(summ) do x
+            push!(all_summaries, x)
+        end
+        if schedule_now
+            i = 0
+            while !istaskstarted(task) && i < 10
+                #println("Waiting for task...")
+                sleep(0.1)
+                i += 1
+            end
+            @test istaskstarted(task)
+        else
+            @test !istaskstarted(task)
+            schedule(task)
+        end
+        wait(task)
+
+        a,b,c = eachrow(reduce(hcat, all_summaries))
+        ua,ub,uc = unique.((a,b,c))
+        ra,rb,rc = r
+
+        @test all(!isnothing, match.(ra,ua))
+        @test all(!isnothing, match.(rb,ub))
+        @test all(!isnothing, match.(rc,uc))
+        for (x,ux) in ((a,ua),(b,ub),(c,uc))
+            @test all(in((0,1)), diff(indexin(x,ux)))
+        end
+    end
+
     v = ["a", "b", "c"]
     args = worker_arg ? (default_worker_pool(), v) : (v,)
     summ, task = opmap(args...; schedule_now=schedule_now,
                     summarize=_summ, on_error=identity) do setmessage, x
         for i in 1:3
-            #println(i)
             setmessage("$x $i")
+        end
+    end
+
+    test_pmap_output(summ, task, v,
+            [[r"^$", r"^a 1$", r"^a 2$", r"^a 3$"],
+             [r"^$", r"^b 1$", r"^b 2$", r"^b 3$"],
+             [r"^$", r"^c 1$", r"^c 2$", r"^c 3$"]],
+    )
+
+    summ, task = ologpmap(args...; schedule_now=schedule_now,
+                    summarize=_summ, on_error=identity) do x
+        for i in 1:3
+            @info "$x $i"
             x == 2 && error()
-            #sleep(rand())
         end
     end
+    test_pmap_output(summ, task, v,
+            [[r"^$", r"┌ Info: a 1\n└ @ Main.*$",
+                     r"┌ Info: a 2\n└ @ Main.*$",
+                     r"┌ Info: a 3\n└ @ Main.*$"],
+             [r"^$", r"┌ Info: b 1\n└ @ Main.*$",
+                     r"┌ Info: b 2\n└ @ Main.*$",
+                     r"┌ Info: b 3\n└ @ Main.*$"],
 
-    all_summaries = []
-    on(summ) do x
-        push!(all_summaries, x)
-    end
-    if schedule_now
-        i = 0
-        while !istaskstarted(task) && i < 10
-            #println("Waiting for task...")
-            sleep(0.1)
-            i += 1
-        end
-        @test istaskstarted(task)
-    else
-        @test !istaskstarted(task)
-        schedule(task)
-    end
-    wait(task)
+             [r"^$", r"┌ Info: c 1\n└ @ Main.*$",
+                     r"┌ Info: c 2\n└ @ Main.*$",
+                     r"┌ Info: c 3\n└ @ Main.*$"]]
+    )
 
-    a,b,c = eachrow(reduce(hcat, all_summaries))
-    ua,ub,uc = unique.((a,b,c))
+end
 
-    @test ua == ["", "a 1", "a 2", "a 3"]
-    @test ub == ["", "b 1", "b 2", "b 3"]
-    @test uc == ["", "c 1", "c 2", "c 3"]
-    for (x,ux) in ((a,ua),(b,ub),(c,uc))
-        @test all(in((0,1)), diff(indexin(x,ux)))
+summ, task = ologpmap(["a", "b", "c"]; schedule_now=false,
+                summarize=_summ, on_error=identity) do x
+    for i in 1:3
+        @info "$x $i"
     end
 end
